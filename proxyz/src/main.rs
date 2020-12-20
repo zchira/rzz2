01fe0006998c98e7ee7c9ebe94eb2a44fa5f384a
zchira <zchira@gmail.com>
2020-12-21 00:05:40 +0100

    proxyz bugs fixed

--- proxyz/src/main.rs
+++ proxyz/src/main.rs
@@ -1,22 +1,25 @@
-use actix_web::{get, web, App, HttpServer, HttpRequest, HttpResponse, Responder};
-use actix_web::client::{Client, ClientBuilder};
-use url::{Url, ParseError};
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse, Responder};
+// use actix_web::client::{Client, ClientBuilder};
+// use url::{Url, ParseError};
 use reqwest;
 
 // #[get("/url/{tail:.*}")]
 async fn index(req: HttpRequest) -> impl Responder {
-    let url_string = &req.uri().to_string()[1..]; // Url::parse(&url.to_string()).unwrap();
+    let url_string = &req.uri().to_string()[6..]; // Url::parse(&url.to_string()).unwrap();
+    println!("__URL: {}", url_string);
 
     // let client = ClientBuilder::new().max_redirects(10).finish();
     // let response = client.get(url_string).send().await;
     let body = reqwest::get(url_string).await.unwrap().text().await.unwrap();
-
-    HttpResponse::Ok().body(format!("{:?}", body))
+    HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/{tail:.*}").to(index)))
+    HttpServer::new(|| App::new()
+        .service(web::resource("/cors/{tail:.*}").to(index))
+        .service(actix_files::Files::new("/", "../frontend/dist").index_file("index.html"))
+        )
     .bind("0.0.0.0:4040")?
         .run()
         .await
