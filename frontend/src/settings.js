9fd22a6ea62b2dce458f767152bae45a97b17059
zc <zchira@gmail.com>
2021-03-17 23:40:04 +0100

    added proxyz azure address

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,8 +1,11 @@
 let CORS_PROXY = '/cors/';
 
-if (process.env.NODE_ENV === 'development') {
+CORS_PROXY = 'https://proxyz.azurewebsites.net/api/corso?code=nBe75gJ1YKNg3XGhEjVoZbhSXyTTs6KbPDR1vaBagRoJLQiqz0qlfw==&q=';
+/* if (process.env.NODE_ENV === 'development') {
     // CORS_PROXY = 'http://127.0.0.1:4040/cors/';
     CORS_PROXY = 'http://127.0.0.1:4040/cors/';
-}
+} else {
+    CORS_PROXY = 'https://proxyz.azurewebsites.net/api/corso?code=nBe75gJ1YKNg3XGhEjVoZbhSXyTTs6KbPDR1vaBagRoJLQiqz0qlfw==&q=';
+} */
 
 export { CORS_PROXY };
